4f03c6b843e2d3bbba2efbc2d4d89a165ad5dbb5
shvetsovdm <sorrows@mail.ru>
2017-03-07 19:16:29 +1000

    Add raw solution

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
+var qr = require('qr-image');
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    var hostURL = req.protocol + '://' + req.get('host') + req.originalUrl;
+    var qrCodeSvgString = qr.imageSync(hostURL + 'pr', { type: 'svg' });
+
+    res.render('index', {
+        title: 'Scan QR code with your mobile device to know your platform',
+        qrCodeSvgString: qrCodeSvgString
+    });
 });
+
+router.get('/pr', function(req, res, next) {
+    var userAgent = req.get('user-agent');
+    var platform = PlarformDetector.fromUserAgetnStr(userAgent)
+
+    res.redirect(302, PlarformRedirects.urlFor(platform));
+});
+
+router.get('/not-supported-platform', function(req, res, next) {
+    res.render('not-supported-platform');
+});
+
+var PlarformDetector = {
+    fromUserAgetnStr: function(userAgentStr) {
+        return userAgentStr.match(/OS X|Android/)[0];
+    }
+}
+
+var PlarformRedirects = {
+    'OS X': 'https://itunes.apple.com',
+    'Android': 'https://play',
+    'Not supported': '/not-supported-platform',
+
+    urlFor: function(platformStr) {
+        return PlarformRedirects[platformStr] || PlarformRedirects['Not supported']
+    }
+}
 
 module.exports = router;
